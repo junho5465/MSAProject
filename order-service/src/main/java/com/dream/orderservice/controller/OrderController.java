ca5712906666a7d32878fa14d66dab12a7861637
phm940916@gmail.com <phm940916@gmail.com>
2022-03-06 22:41:00 +0900

    order 컨트롤러 추가

--- order-service/src/main/java/com/dream/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/dream/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.dream.orderservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
